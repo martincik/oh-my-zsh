55480ba815d58093e3ca8ca2389ad1c4e345cda4
ladislav.martincik <ladislav.martincik@xing.com>
2010-02-15 23:32:00 +0100

    Snow Leopard changes of arch.

--- path.zsh
+++ path.zsh
@@ -1,4 +1,5 @@
-path=(~/bin /opt/local/bin /usr/local/git/libexec/git-core /usr/local/ruby187pl202patched/bin $RIPDIR/active/bin /opt/local/sbin /usr/local/mysql/bin /Users/ladislav.martincik/Projects/playground/code_swarm/bin $path)
+path=(~/bin /opt/local/bin /usr/local/git/libexec/git-core /usr/local/ruby187pl202patched/bin $RIPDIR/active/bin /opt/local/sbin /usr/local/mysql/bin /Users/ladislav.martincik/Projects/playground/code_swarm/bin ~/.bundle/ruby/1.8/bin $path)
 
 EVENT_NOKQUEUE=1
 export RIPDIR RUBYLIB EVENT_NOKQUEUE
+export ARCHFLAGS="-arch x86_64"
